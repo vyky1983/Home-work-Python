7dc0d2174830b58b8160ebe24bbc72103e4dc7d4
vyky1983 <veronica.cernenchii@mail.ru>
2022-12-22 20:30:57 +0300

    add new inf in folder Syminar8

---
+++ Syminar8/init.py
@@ -0,0 +1,13 @@
+from read_data import read_data
+
+def init():
+    if not (len(read_data()) > 0):
+        
+        with open("name.csv", 'a+') as file:
+            file.write("id;surname;name;class;status\n")
+
+        with open("adress.csv", 'a+') as file:
+            file.write("id;city;street;house;apartament;other\n")
+
+        with open("class.csv", 'a+') as file:
+            file.write("id;row;col\n")
